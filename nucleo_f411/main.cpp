1a63223c39ded1b468d78ce8505d74fae2984ee6
talpachen <talpachen@gmail.com>
2021-11-18 07:52:48 +0800

    iar project for stm32f411

---
+++ nucleo_f411/main.cpp
@@ -0,0 +1,29 @@
+/* mbed Microcontroller Library
+ * Copyright (c) 2019 ARM Limited
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "mbed.h"
+#include "platform/mbed_thread.h"
+
+
+// Blinking rate in milliseconds
+#define BLINKING_RATE_MS                                                    500
+
+const uint8_t flash_fill[450 * 1024] = {0, 1, 2, 3, 4};
+
+int main()
+{
+    for (int i = 0; i += 1024; i < sizeof(flash_fill)) {
+        if (flash_fill[i])
+            __ASM("NOP");
+    }
+    
+    // Initialise the digital pin LED1 as an output
+    DigitalOut led(LED1);
+
+    while (true) {
+        led = !led;
+        thread_sleep_for(BLINKING_RATE_MS);
+    }
+}
